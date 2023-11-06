ef4e55490dd1080373c1acf1acdec1ef2ebac99d
Armon <armon@nudgex.net>
2023-11-05 21:44:33 -0800

    added manager and game tables

--- app/models.py
+++ app/models.py
@@ -2,6 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Manager(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    games = db.relationship('Game', backref='manager', lazy=True)
+
+class Game(db.Model):
+    id = db.Column(db.String(4), primary_key=True)
+    manager_id = db.Column(db.Integer, db.ForeignKey('manager.id'), nullable=False)
+
 class DemoModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String)
