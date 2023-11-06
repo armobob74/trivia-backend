4d6dd22dc8529b46dfe24b71de5e13549216e8cd
Armon <armon@nudgex.net>
2023-11-06 00:35:49 -0800

    added cors for socketio too

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,15 @@ from flask_socketio import SocketIO
 from .models import db
 from flask_cors import CORS
 
-socketio = SocketIO()
 
+socketio = SocketIO()
 def create_app(debug=False, test_mode=False):
     """Create an application."""
     app = Flask(__name__)
     app.debug = debug
     app.config['SECRET_KEY'] = 'rijgsiejfies'
-    app.config['FRONTEND_URL'] = 'http://localhost:5173'
+    FRONTEND_URL = 'http://localhost:5173'
+    app.config['CORS_ALLOWED_ORIGINS'] = [FRONTEND_URL]
     if test_mode:
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     else:
@@ -21,9 +22,8 @@ def create_app(debug=False, test_mode=False):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    socketio.init_app(app)
+    CORS(app, supports_credentials=True,origins=app.config['CORS_ALLOWED_ORIGINS'])
+    socketio.init_app(app, cors_allowed_origins=app.config['CORS_ALLOWED_ORIGINS'])
     with app.app_context():
         db.create_all()
-
-    CORS(app, supports_credentials=True,origins=[app.config['FRONTEND_URL']])
     return app
