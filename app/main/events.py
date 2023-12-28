9e2c14439f805d39486c4583303d43d3abfed95f
Armon <armon@nudgex.net>
2023-12-28 00:08:47 -0800

    got basic trivia game working

--- app/main/events.py
+++ app/main/events.py
@@ -1,4 +1,5 @@
 import pdb
+import json
 from flask import session
 from flask_socketio import emit, join_room, leave_room, close_room
 from ..models import Game, db, Manager, Player
@@ -14,10 +15,15 @@ def manageGame(message):
     game_id = message['game-id']
     room = f"manage-{game_id}"
     join_room(room)
-    players = Game.query.get(game_id).players
-    player_info = [{'username':p.username} for p in players]
+    game = Game.query.get(game_id)
+    players = game.players
+    player_info = [{'username':p.username, 'score':p.score,'answer':p.answer, 'answer_is_correct':p.answer_is_correct} for p in players]
     print('manager connected')
-    response = {'text':f'Managing game {game_id}','players':player_info}
+    response = {
+            'text':f'Managing game {game_id}',
+            'players':player_info,
+            'question_idx':game.question_index
+            }
     print(response)
     emit('manage-game-response',response, room=room)
 
@@ -30,9 +36,13 @@ def joinGame(message):
     """
     game_id = message['game-id']
     username = message['username']
+
+    game = Game.query.get(game_id)
+    questions = json.loads(game.questions)
+    question = questions[game.question_index]
     
     join_room(game_id)
-    emit('join-game', {'text': f'joined game {game_id}'})
+    emit('join-game', {'text': f'joined game {game_id}', 'question':question}, room=game_id)
     emit('join-game',{'username':username}, room=f'manage-{game_id}')
 
 @socketio.on('end-game', namespace='/')
@@ -56,7 +66,7 @@ def createManager(message):
 @socketio.on('create-game', namespace='/')
 def createGame(message):
     db.session.add(
-            Game(id=message['game-id'], manager_id=message['manager-id'])
+            Game(id=message['game-id'], manager_id=message['manager-id'], questions=message['questions'])
     )
     db.session.commit()
     emit('create-game-response')
@@ -65,8 +75,89 @@ def createGame(message):
 def createPlayer(message):
     username = message['username']
     game_id = message['game_id']
-    db.session.add(
-            Player(username = username, game_id = game_id)
-            )
+    game = Game.query.get(game_id)
+    if username in [p.username for p in game.players]:
+        emit('create-player-response',{
+            'ok':False,
+            'text': f'Player "{username}" already exists in this game'
+            })
+    else:
+        player = Player(username = username, game_id = game_id)
+        db.session.add(player)
+        db.session.commit()
+        emit('create-player-response', {
+            'ok':True,
+            'text':f'player created with username {username} for game {game_id}',
+            'player-id':player.id
+            })
+
+@socketio.on('next-question', namespace='/')
+def nextQuestion(message):
+    game_id = message['game-id']
+    question = message['question']
+    game = Game.query.get(game_id)
+    game.question_index += 1;
+    for player in game.players:
+        # score is set by front end because front end has accurate info about checkbox condition
+        # answer and answer_is_correct are simultaniously reset on front end and back end on next-question
+
+        player.answer = ''
+        player.answer_is_correct = False
     db.session.commit()
-    emit('create-player-response', {'text':f'player created with username {username} for game {game_id}'})
+    emit('next-question',{'question':question}, room=game_id)
+
+@socketio.on('change-score', namespace='/')
+def changeScore(message):
+    """
+    message fields:
+        game_id
+        username
+        delta
+    """
+    game_id = message['game-id']
+    username = message['username']
+    delta = message['delta']
+    game = Game.query.get(game_id)
+    player = [p for p in game.players if p.username == username][0]
+    player.score += delta;
+    print(username, 'score: ', player.score) 
+    db.session.commit()
+    pass
+    
+
+@socketio.on('submit-answer', namespace='/')
+def submitAnswer(message):
+    """
+    Recieve submission from player and forward the information to manager and database
+    Manager decides correctness of answer
+
+    message fields:
+        username
+        game_id
+        answer
+    """
+    game_id = message['game-id']
+    username = message['username']
+    answer = message['answer']
+    game = Game.query.get(game_id)
+
+    questions = json.loads(game.questions)
+    question = questions[game.question_index]
+    correct_answer = question[question['correct']]
+
+    player_index = [p.username for p in game.players].index(username)
+    player = game.players[player_index]
+    player.answer = answer
+
+    # determine the initial correctnes of answer.
+    # manager has authority to change this by unchecking box in UI
+    # This is necessary for manager console loading
+
+    if answer.lower() == correct_answer.lower():
+        player.answer_is_correct = True
+    else:
+        player.answer_is_correct = False
+    db.session.commit()
+    emit('submit-answer',message,room=f'manage-{game_id}')
+    pass
+    
--- app/models.py
+++ app/models.py
@@ -10,9 +10,13 @@ class Game(db.Model):
     id = db.Column(db.String(4), primary_key=True)
     manager_id = db.Column(db.Integer, db.ForeignKey('manager.id'), nullable=False)
     players = db.relationship('Player', backref='game', lazy=True)  # One-to-many relationship with Player
+    question_index = db.Column(db.Integer, default=0)
+    questions = db.Column(db.String)
 
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
     game_id = db.Column(db.String(4), db.ForeignKey('game.id'), nullable=False)  # Foreign key to Game
-
+    score = db.Column(db.Integer, default=0)
+    answer = db.Column(db.String, default='')
+    answer_is_correct = db.Column(db.Boolean, default=False)
